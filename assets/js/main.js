8a2e85a7f8406dcc78ad8db5491ade4de4b33f0b
nuria-marmol <numarbru@gmail.com>
2022-03-08 23:24:47 +0100

    Grid - primeros pasos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,42 @@
-// Importamos los 3 arrays con las fotografías
-import { level1Pics, level2Pics, level3Pics } from "modules/_json-photos.js";
+// Importamos JSONs con las fotografías. También los iconos para el reverso de las tarjetas
+import { level1Pics, level2Pics, level3Pics, allLevelsIcons } from "./modules/_json-images.js";
 
 // Variables
+/* Concatenamos cada array de fotografías para tenerlas duplicadas */
+const level1Pairs = level1Pics.concat(level1Pics);
+const level2Pairs = level2Pics.concat(level2Pics);
+const level3Pairs = level3Pics.concat(level3Pics);
 
+// Capturamos nuestra plantilla para las imágenes y su destino, el div que tiene el grid
+const cardsTemplate = document.querySelector("#cards-template").content.firstElementChild;
+const templateTarget = document.querySelector("#template-target");
 
 // Funciones
+function shufflePics(arrayPairs) {
+    arrayPairs.sort(function() {
+        // Partimos de la posición del medio. Irá distribuyendo adelante o atrás
+        return 0.5 - Math.random();
+    })
+    return arrayPairs;
+}
+
+function renderLevel(arrayPairs) {
+    const shuffledImages = shufflePics(arrayPairs);
+    shuffledImages.forEach(function(object) {
+        // Clonamos la plantilla
+        const templateCopy = cardsTemplate.cloneNode(true);
+        // Capturamos dentro de ella
+        const cardsBack = templateCopy.querySelector("#cards-back");
+        const cardsFront = templateCopy.querySelector("#cards-front");
+        // Rellenamos
+        cardsFront.setAttribute("src", object.name);
+        cardsFront.setAttribute("alt", object.description);
+        // Movemos la plantilla a su destino
+        templateTarget.appendChild(templateCopy);
+    })
+}
+
+renderLevel(level1Pairs);
 
 
 // Eventos
---
+++ assets/js/modules/_json-images.js
@@ -0,0 +1,119 @@
+// 3 variables modificables con las fotografías para los 3 niveles de cartas
+/* Tendremos que concatenar cada array con el mismo para tener las imágenes duplicadas. */
+export const level1Pics = [
+    {
+        // Ruta pensando en que tendrá que llegar a ellas desde index.html
+        name: "assets/img/level1/ferris-wheel.jpg",
+        description: "Ferris wheel"
+    },
+    {
+        name: "assets/img/level1/harbour.jpg",
+        description: "Harbour at night"
+    },
+    {
+        name: "assets/img/level1/psychedelic.jpg",
+        description: "Psychedelic lights on a path"
+    },
+    {
+        name: "assets/img/level1/smoke.jpg",
+        description: "Blue smoke at night"
+    },
+    {
+        name: "assets/img/level1/stars.jpg",
+        description: "Paper star lights"
+    }
+]
+
+export const level2Pics= [
+    {
+        name: "assets/img/level2/bridge.jpg",
+        description: "Bridge"
+    },
+    {
+        name: "assets/img/level2/church.jpg",
+        description: "Church"
+    },
+    {
+        name: "assets/img/level2/deer.jpg",
+        description: "Colorfull graffiti of a deer"
+    },
+    {
+        name: "assets/img/level2/elephants.jpg",
+        description: "Graffiti of elephants"
+    },
+    {
+        name: "assets/img/level2/houses.jpg",
+        description: "Houses"
+    },
+    {
+        name: "assets/img/level2/phone-box.jpg",
+        description: "Typical red phone box"
+    },
+    {
+        name: "assets/img/level2/window.jpg",
+        description: "Window"
+    },
+    {
+        name: "assets/img/level2/woman.jpg",
+        description: "Graffiti of a woman"
+    }
+]
+
+export const level3Pics = [
+    {
+        name: "assets/img/level3/bellflowers.jpg",
+        description: "Purple bellflowers"
+    },
+    {
+        name: "assets/img/level3/branches.jpg",
+        description: "Branches from a tree"
+    },
+    {
+        name: "assets/img/level3/daisies.jpg",
+        description: "Yellow daisies"
+    },
+    {
+        name: "assets/img/level3/electricity-pylon.jpg",
+        description: "Electricity pylon on a landscape"
+    },
+    {
+        name: "assets/img/level3/leaves.jpg",
+        description: "Leaves on grass"
+    },
+    {
+        name: "assets/img/level3/path.jpg",
+        description: "Path from a natural park"
+    },
+    {
+        name: "assets/img/level3/river.jpg",
+        description: "River"
+    },
+    {
+        name: "assets/img/level3/seeds.jpg",
+        description: "Seeds"
+    },
+    {
+        name: "assets/img/level3/water-lilies.jpg",
+        description: "Pond with water lilies"
+    },
+    {
+        name: "assets/img/level3/yellow-flower.jpg",
+        description: "Yellow flower"
+    }
+]
+
+// Esta no la tendremos que concatenar
+export const allLevelsIcons = [
+    {
+        name: "assets/img/icons/moon.svg",
+        description: "Half Moon"
+    },
+    {
+        name: "assets/img/icons/maze.svg",
+        description: "Simple maze"
+    },
+    {
+        name: "assets/img/icons/flower.svg",
+        description: "Minimalist flower"
+    }
+]
